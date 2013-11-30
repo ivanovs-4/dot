e311121a8a925d1f7b746682c04bc8214a31bc47
Sergey Ivanov <seiv@yandex-team.ru>
2013-11-30 20:23:41 +0600

    deploy & remember

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cp ~/dot/bash_aliases ~/.bash_aliases
+cp ~/dot/bash_aliases_git ~/.bash_aliases_git
+cp ~/dot/bashrc.post ~/.bashrc.post
+cp ~/dot/gitconfig ~/.gitconfig
+cp ~/dot/gitignore_global ~/.gitignore_global
+cp ~/dot/tmux.conf ~/.tmux.conf
+cp ~/dot/vimrc ~/.vimrc
+cp ~/dot/Xmodmap ~/.Xmodmap
+cp ~/dot/zshrc ~/.zshrc
+
---
+++ remember.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cp ~/.bash_aliases ~/dot/bash_aliases
+cp ~/.bash_aliases_git ~/dot/bash_aliases_git
+cp ~/.bashrc.post ~/dot/bashrc.post
+cp ~/.gitconfig ~/dot/gitconfig
+cp ~/.gitignore_global ~/dot/gitignore_global
+cp ~/.tmux.conf ~/dot/tmux.conf
+cp ~/.vimrc ~/dot/vimrc
+cp ~/.Xmodmap ~/dot/Xmodmap
+cp ~/.zshrc ~/dot/zshrc
+
+git status
+
