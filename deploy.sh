d445f628d27bb6c19a210a2d3f315221a089833c
Sergey Ivanov <seiv@yandex-team.ru>
2013-12-04 05:16:55 +0400

    vimrc && deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-ln -s ~/dot/bash_aliases ~/.bash_aliases
-ln -s ~/dot/bash_aliases_git ~/.bash_aliases_git
-ln -s ~/dot/bashrc.post ~/.bashrc.post
-ln -s ~/dot/gitconfig ~/.gitconfig
-ln -s ~/dot/gitignore_global ~/.gitignore_global
-ln -s ~/dot/tmux.conf ~/.tmux.conf
-ln -s ~/dot/vim ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/dot/Xmodmap ~/.Xmodmap
-ln -s ~/dot/zshrc ~/.zshrc
-ln -s ~/dot/git_template ~/.git_template
+ln -sf ~/dot/bash_aliases ~/.bash_aliases
+ln -sf ~/dot/bash_aliases_git ~/.bash_aliases_git
+ln -sf ~/dot/bashrc.post ~/.bashrc.post
+ln -sf ~/dot/gitconfig ~/.gitconfig
+ln -sf ~/dot/gitignore_global ~/.gitignore_global
+ln -sf ~/dot/tmux.conf ~/.tmux.conf
+rm -f ~/.vim
+ln -sf ~/dot/vim ~/.vim
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/dot/Xmodmap ~/.Xmodmap
+ln -sf ~/dot/zshrc ~/.zshrc
+rm -f ~/.git_template
+ln -sf ~/dot/git_template ~/.git_template
+rm -f ~/.i3
+ln -sf ~/dot/i3 ~/.i3
 
