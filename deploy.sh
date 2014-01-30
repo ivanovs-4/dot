dcf36c1a6353ad1030e4718850c6a025b5976ba2
Sergey Ivanov <seiv@yandex-team.ru>
2014-01-30 22:37:55 +0600

    gitignore added

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,18 @@ ln -sf ~/dot/gitconfig ~/.gitconfig
 ln -sf ~/dot/gitignore_global ~/.gitignore_global
 ln -sf ~/dot/tmux.conf ~/.tmux.conf
 
-rm -f ~/.vim
+rm -rf ~/.vim
 ln -sf ~/dot/vim ~/.vim
 
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/dot/Xmodmap ~/.Xmodmap
 ln -sf ~/dot/zshrc ~/.zshrc
 
-rm -f ~/.git_template
+rm -rf ~/.git_template
 ln -sf ~/dot/git_template ~/.git_template
 
-rm -f ~/.i3
+rm -rf ~/.i3
 ln -sf ~/dot/i3 ~/.i3
-
-rm -f ~/.git_template
-ln -sf ~/dot/git_template ~/.git_template
 
 git submodule init
 git submodule update
--- vim/bundle/vim-python-scripting/plugin/vim-python-scripting.vim
+++ vim/bundle/vim-python-scripting/plugin/vim-python-scripting.vim
@@ -2,6 +2,7 @@ python << endpython
 import sys
 
 PLUGIN_PATH = '/home/seiv/.vim/bundle/vim-python-scripting/plugin/'
+PLUGIN_PATH = '/Users/seiv/.vim/bundle/vim-python-scripting/plugin/'
 
 if PLUGIN_PATH not in sys.path:
     sys.path.append(PLUGIN_PATH )
