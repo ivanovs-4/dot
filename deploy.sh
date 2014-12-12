da76e74a8ac5161c2b6d63179df0f47778a3871b
Sergey Ivanov <seiv@yandex-team.ru>
2014-12-12 15:57:28 +0300

    gitconfig diffconflicts

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,15 @@ ln -sf ~/dot/gitconfig ~/.gitconfig
 ln -sf ~/dot/gitignore_global ~/.gitignore_global
 ln -sf ~/dot/tmux.conf ~/.tmux.conf
 
+
 rm -rf ~/.vim
 ln -sf ~/dot/vim ~/.vim
 
 ln -sf ~/.vim/vimrc ~/.vimrc
+
+mkdir -p ~/bin
+ln -s ~/.vim/bin/diffconflicts ~/bin/
+
 
 rm -rf ~/.git_template
 ln -sf ~/dot/git_template ~/.git_template
